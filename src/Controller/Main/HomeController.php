292979d682e3d9efc2fa8cf861ccb3a03fb000c0
Mikk78 <teshcenko93@gmail.com>
2021-02-28 15:50:05 +0100

    create BaseController and HomeController

---
+++ src/Controller/Main/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller\Main;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends BaseController
+{
+    /**
+     * Use annotation
+     * @Route("/",name="home")
+     */
+    public function index()
+    {
+        $forRender = parent::renderDefault();//inheritance method of BaseController
+        return $this->render("main/index.html.twig",$forRender);
+    }   
+}
